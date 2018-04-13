1e2fc09fdcc94f6b55b0486e8d1b328baeb83c8d
Tomer Weller <tomer.weller@gmail.com>
2018-04-13 13:56:36 -0700

    add tx_count to block monitor

--- block_monitor.py
+++ block_monitor.py
@@ -6,7 +6,7 @@ INTERVAL = 0.1
 
 def monitor_block_timestamps(csv_out, interval):
     """gather block information to csv.
-    per block: block_number, block_timestamp (by miner), block_timestamp (by me), delta of both
+    per block: block_number, block_timestamp (by miner), block_timestamp (by me), delta of both, tx_count
     """
     w3 = get_w3()  # a hacky way to overcome sync issues with w3
     blocks = {}
@@ -18,7 +18,8 @@ def monitor_block_timestamps(csv_out, interval):
         if latest_block_number not in blocks:
             log(f"new block detected: {latest_block_number}")
             blocks[latest_block_number] = latest_block_timestamp
-            row = [latest_block_number, latest_block_timestamp, my_timestamp, my_timestamp - latest_block_timestamp]
+            row = [latest_block_number, latest_block_timestamp, my_timestamp, my_timestamp - latest_block_timestamp,
+                   len(latest_block.transactions)]
             csv_out.append(row)
             log(row)
         time.sleep(interval)
@@ -26,5 +27,6 @@ def monitor_block_timestamps(csv_out, interval):
 
 if __name__ == "__main__":
     block_csv_writer = CSVWriter(f"results/blocks.{now_str()}.csv",
-                                 ["block_number", "block_timestamp", "my_timestamp", "delta"])
+                                 ["block_number", "block_timestamp", "my_timestamp", "timestamp_delta", "tx_count",
+                                  "avg_gas_price"])
     monitor_block_timestamps(block_csv_writer, INTERVAL)
