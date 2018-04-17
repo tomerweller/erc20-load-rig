b11b1101e1ae0e5197b47a7400f4e35f9cd9a72a
Tomer Weller <tomer.weller@gmail.com>
2018-04-17 11:31:16 -0700

    account_cleanup robsutness

--- account_cleanup.py
+++ account_cleanup.py
@@ -1,8 +1,27 @@
 import time
 
-from common import w3, Account, send_ether, GAS_LIMIT, funder, get_gas_price, get_arg, log
+from common import w3, Account, send_ether, funder, get_gas_price, get_arg, log
+from web3.utils.threads import Timeout
 
 INTERVAL = 1
+
+
+def get_transaction_count(address):
+    while True:
+        try:
+            return w3.eth.getTransactionCount(address)
+        except Timeout as e:
+            log(f"timeout in get_transaction_count. sleeping a bit. ({e})")
+            time.sleep(INTERVAL)
+
+
+def get_balance(address):
+    while True:
+        try:
+            return w3.eth.getBalance(address)
+        except Timeout as e:
+            log(f"timeout in get_balance. sleeping a bit. ({e})")
+            time.sleep(INTERVAL)
 
 
 def cleanup(csv_in):
@@ -17,10 +36,12 @@ def cleanup(csv_in):
 
     for line in lines[1:]:
         account = Account.privateKeyToAccount(line.split(',')[0])
-        nonce = w3.eth.getTransactionCount(account.address)
+
+        nonce = get_transaction_count(account.address)
         balance = w3.eth.getBalance(account.address)
-        if balance > gas_limit * gas_price:
-            tx_hash = send_ether(account, nonce, funder.account.address, balance - gas_limit * gas_price, gas_price, gas_limit)
+        if balance >= gas_limit * gas_price:
+            tx_hash = send_ether(account, nonce, funder.account.address, balance - gas_limit * gas_price, gas_price,
+                                 gas_limit)
             log(f"{account.address}, {nonce}, {balance}, {tx_hash}")
         else:
             log(f"balance too low: {balance}")
