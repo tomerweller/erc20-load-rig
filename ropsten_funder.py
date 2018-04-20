ae352992b21724d2f71e2ce924127c5fbebff496
Tomer Weller <tomer.weller@gmail.com>
2018-04-19 19:31:56 -0700

    fix ropsten funder to use connection adapter

--- ropsten_funder.py
+++ ropsten_funder.py
@@ -1,12 +1,16 @@
 import requests
 import time
 
-from common import create_account, send_ether, funder, GAS_PRICE, GAS_LIMIT, get_gas_price
+from common import get_gas_price, get_env_connection, get_env_funder
+
+GAS_LIMIT = 21000
 
 
 def main():
+    conn = get_env_connection()
+    funder = get_env_funder(conn)
     while True:
-        tmp = create_account()
+        tmp = conn.create_account()
         r = requests.get('http://faucet.ropsten.be:3001/donate/' + tmp.account.address)
         print(r.status_code, r.json())
         if r.status_code == 200:
@@ -14,8 +18,10 @@ def main():
                 print(".", end='', flush=True)
                 time.sleep(1)
             print("\nfunder balance: " + str(funder.balance()))
-            gas_price =  get_gas_price("fastest")
-            tx_hash = send_ether(tmp.account, 0, funder.account.address, 1000000000000000000 - gas_price * GAS_LIMIT,)
+            gas_price = get_gas_price("fastest")
+            tx_hash = conn.send_ether(tmp.account, 0,
+                                      funder.account.address, r.json()["amount"] - gas_price * GAS_LIMIT,
+                                      gas_price, GAS_LIMIT)
             print(tmp.account.address, tx_hash)
         else:
             time.sleep(1)
